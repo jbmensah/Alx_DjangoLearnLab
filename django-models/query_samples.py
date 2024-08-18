c59ac759ba38776335bef9c5960f0627e02d04fd
jbmensah <jaybmensah@gmail.com>
2024-08-18 23:47:11 +0000

    Add query_sample

--- django-models/query_samples.py
+++ django-models/query_samples.py
@@ -1,46 +1,52 @@
 from relationship_app.models import Author, Book, Library, Librarian
 
-# Create Authors
-jk_rowling = Author.objects.create(name="J.K. Rowling")
+# Add Author objects
 george_rr_martin = Author.objects.create(name="George R.R. Martin")
+george_rr_martin.save()
 james_clear = Author.objects.create(name="James Clear")
+james_clear.save()
 
-# Create Books by J.K. Rowling
-book1 = Book.objects.create(title="Harry Potter and the Sorcerer's Stone", author=jk_rowling)
-book2 = Book.objects.create(title="Harry Potter and the Chamber of Secrets", author=jk_rowling)
+# Add Book objects
+game_of_thrones = Book.objects.create(title="A Game of Thrones", author=george_rr_martin)
+clash_of_kings = Book.objects.create(title="A Clash of Kings", author=george_rr_martin)
+atomic_habits = Book.objects.create(title="Atomic Habits", author=james_clear)
 
-# Create Books by George R.R. Martin
-book3 = Book.objects.create(title="A Game of Thrones", author=george_rr_martin)
-book4 = Book.objects.create(title="A Clash of Kings", author=george_rr_martin)
+game_of_thrones.save()
+clash_of_kings.save()
+atomic_habits.save()
 
-# Create Books by James Clear
-book5 = Book.objects.create(title="Atomic Habits", author=james_clear)
+# Get books by one author
+print("Books by George R.R. Martin:")
+george_rr_martin = Author.objects.get(name="George R.R. Martin")
+books_by_george = Book.objects.filter(author=george_rr_martin)
+for book in books_by_george:
+    print(book.title)
 
-# Create a Library
-accra_library = Library.objects.create(name="Accra Central Library")
+# List all books
+print("\nAll books:")
+all_books = Book.objects.all()
+for book in all_books:
+    print(book.title)
 
-# Add books to the library
-accra_library.book.add(book1, book2, book3, book4, book5)
+# Create Library Instance
+accra_library = Library.objects.create(name='Accra Central Library')
+accra_library.save()
 
-# Create a Librarian for the Library
-librarian = Librarian.objects.create(name="John Doe", library=accra_library)
+# Add books to Library
+accra_library.book.add(game_of_thrones, clash_of_kings, atomic_habits)
 
-# Query books by J.K. Rowling
-print("Books by J.K. Rowling:")
-author = Author.objects.get(name="J.K. Rowling")
-books = Book.objects.filter(author=author)
-for book in books:
-	print(book.title)
-
-# List all books in Accra Central Library
+# List all books in Library
 print("\nBooks in Accra Central Library:")
-library = Library.objects.get(name="Accra Central Library")
-books = library.book.all()  # Use `.all()` to retrieve all books
-for book in books:
-	print(book.title)
-	
-# Retrieve the librarian for Accra Central Library
+library = Library.objects.get(name='Accra Central Library')
+books_in_library = library.book.all()
+for book in books_in_library:
+    print(book.title)
+
+# Add Librarian
+librarian = Librarian.objects.create(name='Akua Sekyiwaa', library=accra_library)
+librarian.save()
+
+# Retrieve a Librarian for a library
 print("\nLibrarian for Accra Central Library:")
-library = Library.objects.get(name="Accra Central Library")
-librarian = Librarian.objects.get(library=library)
+librarian = Librarian.objects.get(library=accra_library)
 print(librarian.name)
