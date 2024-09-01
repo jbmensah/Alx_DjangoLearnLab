4e56c0a1bee4649b44cb1f202398958a67d28f75
jbmensah <jaybmensah@gmail.com>
2024-09-01 21:40:24 +0000

    Managing Permissions and Groups in Django

--- advanced_features_and_security/LibraryProject/bookshelf/views.py
+++ advanced_features_and_security/LibraryProject/bookshelf/views.py
@@ -1,3 +1,46 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404, redirect
+from django.contrib.auth.decorators import login_required, permission_required
+from django.http import HttpResponseForbidden
+from .models import Book
 
-# Create your views here.
+# View for listing books - anyone logged in can view
+@login_required
+@permission_required('app_name.can_view', raise_exception=True)
+def book_list(request):
+	books = Book.objects.all()
+	return render(request, 'books/book_list.html', {'books': books})
+
+# View for adding a new book - only users with can_create permission
+@login_required
+@permission_required('app_name.can_create', raise_exception=True)
+def book_create(request):
+	if request.method == 'POST':
+		title = request.POST.get('title')
+		author = request.POST.get('author')
+		publication_year = request.POST.get('publication_year')
+		Book.objects.create(title=title, author=author, publication_year=publication_year)
+		return redirect('book_list')
+	return render(request, 'books/book_form.html')
+
+# View for editing an existing book - only users with can_edit permission
+@login_required
+@permission_required('app_name.can_edit', raise_exception=True)
+def book_edit(request, pk):
+	book = get_object_or_404(Book, pk=pk)
+	if request.method == 'POST':
+		book.title = request.POST.get('title')
+		book.author = request.POST.get('author')
+		book.publication_year = request.POST.get('publication_year')
+		book.save()
+		return redirect('book_list')
+	return render(request, 'books/book_form.html', {'book': book})
+
+# View for deleting a book - only users with can_delete permission
+@login_required
+@permission_required('app_name.can_delete', raise_exception=True)
+def book_delete(request, pk):
+	book = get_object_or_404(Book, pk=pk)
+	if request.method == 'POST':
+		book.delete()
+		return redirect('book_list')
+	return render(request, 'books/book_confirm_delete.html', {'book': book})
