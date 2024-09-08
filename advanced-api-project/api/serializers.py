45d9905d9d840afd1b806f1e06d6b42dba5494f3
jbmensah <jaybmensah@gmail.com>
2024-09-08 21:55:02 +0000

    Create custom serializers that handle complex data structures

---
+++ advanced-api-project/api/serializers.py
@@ -0,0 +1,25 @@
+from rest_framework import serializers
+
+from .models import Author, Book
+
+# Serializes the Book model, including custom validation for publication year
+class BookSerializer(serializers.ModelSerializer):
+	class Meta:
+		model = Book
+		fields = "__all__"
+    
+		# Custom validation for publication year
+		def validate_publication_year(self, value):
+			from datetime import datetime
+			current_year = datetime.now().year
+			if value > current_year:
+				raise serializers.ValidationError("Publication year cannot be in the future.")
+			return value
+
+# Serializes the Author model, including a nested BookSerializer for related books
+class AuthorSerializer(serializers.ModelSerializer):
+	book = BookSerializer(many=True, read_only=True)
+
+	class Meta:
+		model = Author
+		fields = ['name', 'book']
