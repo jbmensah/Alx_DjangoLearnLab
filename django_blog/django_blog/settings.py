b5a560e1fc87da9b56575834c2384f304f21f205
jbmensah <jaybmensah@gmail.com>
2024-09-14 03:37:53 +0000

    Changed DB

--- django_blog/django_blog/settings.py
+++ django_blog/django_blog/settings.py
@@ -77,8 +77,12 @@ WSGI_APPLICATION = 'django_blog.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': 'blog_db',
+		'USER': 'root',
+		'PASSWORD': 'T3mp54321!',
+		'HOST': '127.0.0.1',
+		'PORT': '3306',
     }
 }
 
@@ -119,7 +123,9 @@ USE_TZ = True
 
 STATIC_URL = '/static/'
 
-STATICFILES_DIRS = [ BASE_DIR / "static" ]
+STATICFILES_DIRS = [ 
+	BASE_DIR / 'static',
+	]
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.1/ref/settings/#default-auto-field
