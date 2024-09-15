c5305a02da87b70a8a4353a06754bff0e421100f
jbmensah <jaybmensah@gmail.com>
2024-09-15 21:49:47 +0000

    Implementing Advanced Features:modified slug in urls

--- django_blog/blog/urls.py
+++ django_blog/blog/urls.py
@@ -26,5 +26,5 @@ urlpatterns = [
 	path('post/<int:pk>/comments/new/', CommentCreateView.as_view(), name='add_comment'),
     path('comment/<int:pk>/update/', CommentUpdateView.as_view(), name='edit_comment'),
     path('comment/<int:pk>/delete/', CommentDeleteView.as_view(), name='delete_comment'),
-	path('tag/<slug:tag_slug>/', PostByTagListView.as_view(), name='post_by_tag'),
+	path('tags/<slug:tag_slug>/', PostByTagListView.as_view(), name='post_by_tag'),
 ]
