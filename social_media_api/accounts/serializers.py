826d4b6ec1bedbf420e8ce10f8b44d3de48b17c2
jbmensah <jaybmensah@gmail.com>
2024-09-22 21:26:45 +0000

    Project Setup and User Authentication for a Social Media API

--- social_media_api/accounts/serializers.py
+++ social_media_api/accounts/serializers.py
@@ -1,6 +1,8 @@
 from rest_framework import serializers
 from .models import CustomUser
 from django.contrib.auth.password_validation import validate_password
+from rest_framework.authtoken.models import Token  # Import Token
+from django.contrib.auth import get_user_model
 
 class RegisterSerializer(serializers.ModelSerializer):
 	password = serializers.CharField(write_only=True, required=True, validators=[validate_password])
@@ -9,7 +11,7 @@ class RegisterSerializer(serializers.ModelSerializer):
 	class Meta:
 		model = CustomUser
 		fields = ('username', 'password', 'password2', 'email', 'bio', 'profile_picture')
-		
+	
 	def validate(self, attrs):
 		if attrs['password'] != attrs['password2']:
 			raise serializers.ValidationError({"password": "Password fields didn't match."})
@@ -23,11 +25,15 @@ class RegisterSerializer(serializers.ModelSerializer):
 		)
 		user.set_password(validated_data['password'])
 		user.save()
-		return user
-	
+
+		# Create and return token after the user is created
+		token = Token.objects.create(user=user)  # Token generation
+
+		return user, token  # Return the token along with the user
+
+# Serializer for handling user profile view and update
 class UserProfileSerializer(serializers.ModelSerializer):
-    class Meta:
-        model = CustomUser
-        fields = ['username', 'email', 'bio', 'profile_picture', 'followers']
-        read_only_fields = ['username', 'email', 'followers']
-		
+	class Meta:
+		model = CustomUser
+		fields = ['username', 'email', 'bio', 'profile_picture', 'followers']
+		read_only_fields = ['username', 'email', 'followers']
--- social_media_api/accounts/views.py
+++ social_media_api/accounts/views.py
@@ -1,20 +1,46 @@
-from rest_framework import generics, permissions
+from rest_framework.response import Response
+from rest_framework import generics
+from .serializers import RegisterSerializer
+from rest_framework.authtoken.models import Token
+from rest_framework.authtoken.views import ObtainAuthToken
 from .models import CustomUser
-from .serializers import RegisterSerializer, UserProfileSerializer
-from rest_framework.permissions import AllowAny
-
+from .serializers import UserProfileSerializer
+from rest_framework.permissions import IsAuthenticated
 
 class RegisterView(generics.CreateAPIView):
-	queryset = CustomUser.objects.all()
-	permission_classes = (AllowAny,)
 	serializer_class = RegisterSerializer
 
+	def create(self, request, *args, **kwargs):
+		serializer = self.get_serializer(data=request.data)
+		serializer.is_valid(raise_exception=True)
+		user, token = serializer.save()
+
+		return Response({
+			"user": {
+				"username": user.username,
+				"email": user.email,
+				"bio": user.bio,
+			},
+			"token": token.key  # Return the token in the response
+		})
+
+
+class LoginView(ObtainAuthToken):
+	def post(self, request, *args, **kwargs):
+		serializer = self.serializer_class(data=request.data, context={'request': request})
+		serializer.is_valid(raise_exception=True)
+		user = serializer.validated_data['user']
+		token, created = Token.objects.get_or_create(user=user)
+		return Response({
+			'token': token.key,
+			'user_id': user.pk,
+			'username': user.username
+		})
 
 class UserProfileView(generics.RetrieveUpdateAPIView):
-    queryset = CustomUser.objects.all()
-    serializer_class = UserProfileSerializer
-    permission_classes = [permissions.IsAuthenticated]
+	queryset = CustomUser.objects.all()
+	serializer_class = UserProfileSerializer
+	permission_classes = [IsAuthenticated]
 
-    def get_object(self):
-        # Override get_object to return the currently authenticated user
-        return self.request.user
+	def get_object(self):
+		return self.request.user  # Return the authenticated user's profile
